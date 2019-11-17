0020fcc231b96a1297ccf04721eaf00fe9a09f5e
Kiran Patel <kiran.jagadish.patel@gmail.com>
2019-11-17 17:36:33 +0000

    deployment fixes

--- ubuntu_deployment/deploy.sh
+++ ubuntu_deployment/deploy.sh
@@ -10,9 +10,11 @@ sudo apt-get -y install supervisor nginx
 # env variable setup
 python3 -m venv venv
 source venv/bin/activate
+pip install wheel
 pip install -r requirements.txt
 SECRET=`python3 -c "import uuid; print(uuid.uuid4().hex)"` 
 echo "SECRET_KEY=$SECRET" >> .env
+export FLASK_APP=guestbook.py
 echo "export FLASK_APP=guestbook.py" >> ~/.profile
 
 # flask db setup !!!!!! TEMP DB SHOULD BE MOVED TO RDS INSTANCE
@@ -23,7 +25,7 @@ flask db upgrade
 cd ../ubuntu_deployment
 
 # flask web setup
-cp guestbook.conf.supervisord /etc/supervisor/conf.d/guestbook.conf
+sudo cp guestbook.conf.supervisord /etc/supervisor/conf.d/guestbook.conf
 sudo supervisorctl reload
 
 #nginx setup
